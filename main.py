4d4e28a5439837521e7ab1c01f1df759826e2c89
Erik Witte <witte.erik2@gmail.com>
2023-08-02 14:40:35 +0200

    fetching title and link done

--- main.py
+++ main.py
@@ -4,25 +4,32 @@ import re
 url = "https://www.welt.de"
 page = urlopen(url)
 html = page.read().decode("UTF-8")
-file = open("test.txt", "w")
-file.write(html)
-file.close()
 patternStart = "<section class=\"c-stage c-stage--headlines\" data-external-stage-name=\"headlines\" data-layout=\"headlines\">"
 patternEnd = "</section>"
-patternSearch = "title="
+patternTitle = "title="
+patternLink = "href="
 headlines = []
 matchesArray = re.findall(f"({patternStart}.*{patternEnd})", html)
 matches = matchesArray[0]
+print(matches)
+indexOverwrite = re.search(patternLink, matches).start()
+matches = matches[:indexOverwrite] + '?' + matches[indexOverwrite+1:]
 for i in range(6):
-        indexOverwrite = re.search(patternSearch, matches).start()
-        indexReadOut = re.search(patternSearch, matches).end()+1
+        indexOverwrite = re.search(patternTitle, matches).start()
+        indexReadTitle = re.search(patternTitle, matches).end()+1
         matches = matches[:indexOverwrite] + '?' + matches[indexOverwrite+1:]
-        toAppend = ""
-        #print(html[indexReadOut])
-        while matches[indexReadOut] != '"':
-            toAppend = toAppend + matches[indexReadOut]
-            indexReadOut += 1
-        #print(re.search(patternSearch, html))
-        headlines.append(toAppend)
+        indexOverwrite = re.search(patternLink, matches).start()
+        indexReadLink = re.search(patternLink, matches).end()+1
+        matches = matches[:indexOverwrite] + '?' + matches[indexOverwrite+1:]
+        titleAppend = ""
+        linkAppend = "https://www.welt.de/"
+        while matches[indexReadTitle] != '"':
+            titleAppend += matches[indexReadTitle]
+            indexReadTitle += 1
+        headlines.append(titleAppend)
+        while matches[indexReadLink] != '"':
+            linkAppend += matches[indexReadLink]
+            indexReadLink += 1
+        headlines[i] += "\n Link: " + linkAppend
 for w in headlines:
     print(w)
